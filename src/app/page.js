c835ae15f12995c2b4b6ade80a6a600dac9cee04
Nikhil Ram <nraminno@gmail.com>
2023-06-26 08:31:30 +0530

    res

--- src/app/page.js
+++ src/app/page.js
@@ -114,7 +114,7 @@ export default function Home() {
               Shortfilm
             </button>
           </div>
-          <div className="grid grid-cols-3 gap-2">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
             {data.data.map((i, index) => {
               if (i.media_type === "IMAGE")
                 return (
